847a661527210d66ada3614ac83f623ef79bc0c3
lee4339 <skyho4339@naver.com>
2022-04-01 12:59:11 +0900

    백엔드(프론트기초수업) 인스타 진행중

--- web/instagram/js/auth.js
+++ web/instagram/js/auth.js
@@ -3,45 +3,46 @@ const username_input = input_items[0].querySelector("input");
 const username_label = input_items[0].querySelector("label");
 const password_input = input_items[1].querySelector("input");
 const password_label = input_items[1].querySelector("label");
-const signin_button = input_items[2].querySelector("button")
+const signin_button = input_items[2].querySelector("button");
 
-username_input.onkeypress = () => {
-    if(window.event.keyCode == 13) {
+username_input.onkeydown = () => {
+    if (window.event.keyCode == 13) {
         password_input.focus();
-    }else {
+    } else {
         username_label.style.top = "2px";
         username_label.style.fontSize = "11px";
     }
 }
 
 username_input.onkeyup = () => {
-    if(username_input.value.length ==0) {
+    if (username_input.value.length == 0) {
         username_label.style.top = "10px";
-        username_label.style.fontSize = "13px"; 
+        username_label.style.fontSize = "13px";
     }
 }
+
 password_input.onkeydown = () => {
-    if(window.event.keyCode == 13) {
+    if (window.event.keyCode == 13) {
         signin_button.click();
-    }else {
+    } else {
         password_label.style.top = "2px";
         password_label.style.fontSize = "11px";
     }
 }
 
 password_input.onkeyup = () => {
-    if(password_input.value.length ==0) {
+    if (password_input.value.length == 0) {
         password_label.style.top = "10px";
-        password_label.style.fontSize = "13px"; 
+        password_label.style.fontSize = "13px";
     }
 }
 
 signin_button.onclick = () => {
-    if(username_input.value.length == 0) {
+    if (username_input.value.length == 0) {
         alert("아이디를 입력해주세요.");
-    }else if(password_input.value.length ==0) {
+    } else if (password_input.value.length == 0) {
         alert("비밀번호를 입력해주세요.");
-    }else {
+    } else {
         alert("로그인 시도");
     }
 }
