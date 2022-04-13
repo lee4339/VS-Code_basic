bb017a5cb59679f161dcf92569c52ac278d2d401
lee4339 <skyho4339@naver.com>
2022-04-14 00:40:13 +0900

    프론트엔드 기초 7일차 복습

--- web/htmlStudy/ex/13/js/test.js
+++ web/htmlStudy/ex/13/js/test.js
@@ -1,4 +1,10 @@
 console.log("노드 JS 데이터 출력 테스트")
+console.log("노드 JS 데이터 출력 테스트")
+console.log("노드 JS 데이터 출력 테스트")
+console.log("노드 JS 데이터 출력 테스트")
+console.log("노드 JS 데이터 출력 테스트")
+console.log("노드 JS 데이터 출력 테스트")
+console.log("노드 JS 데이터 출력 테스트")
 
 let test = 10;
 console.log(test);
